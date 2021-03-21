3ed4a36975b6494b9d70f6bc771432a640fe2286
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-21 11:11:11 -0400

    feat: db_connection.php added

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$server_name = 'localhost:3333';
+$username = 'root';
+$password = '';
+$db = 'login-crud';
+
+try {
+    $conn = new PDO("mysql:host=$server_name;dbname=$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed " . $e->getMessage();
+}
