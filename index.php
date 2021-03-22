25ea65fe612c52e12c42be6686e1c838f93ce678
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-22 18:22:32 -0400

    feat: MaterializeCSS CDN added

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+
+    require 'db_connection.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/normalize.css">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>PHP Login CRUD</title>
+    </head>
+
+    <body>
+
+        <main class="container">
+
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">PHP LOGIN CRUD</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <section class="row">
+                <div class="col s-12">
+                    <h1>PHP Login CRUD (another bored to-do list but with a register and login features)</h1>
+                    <a href="register.php" class="waves-effect waves-light btn-large">Sign Up</a>
+                    <a href="login" class="waves-effect waves-light btn-large">Log In</a>
+                </div>
+            </section>
+        
+        </main>
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    </body>
+
+</html>
